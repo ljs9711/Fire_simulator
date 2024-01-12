362167fb4ce6cca44aa515259c29225f3bb9dc58
ljs9711 <32507881+ljs9711@users.noreply.github.com>
2024-01-12 12:21:49 +0900

    [feat] Asset input

--- Fire_simulator/Assets/Scripts/Player.cs
+++ Fire_simulator/Assets/Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movementSpeed = 5.0f;
+    public float mouseSensitivity = 2.0f;
+
+    private void Update()
     {
-        
+        // 캐릭터 이동
+        MoveCharacter();
+
+        // 카메라 회전
+        RotateCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+    void MoveCharacter()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical) * movementSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 rotation = new Vector3(-mouseY, mouseX, 0.0f) * mouseSensitivity;
+        transform.Rotate(rotation);
+
+        // 수직 회전 제한
+        float currentXRotation = transform.rotation.eulerAngles.x;
+        if (currentXRotation > 90.0f && currentXRotation < 180.0f)
+        {
+            currentXRotation = 90.0f;
+        }
+        else if (currentXRotation > 180.0f && currentXRotation < 270.0f)
+        {
+            currentXRotation = 270.0f;
+        }
+
+        transform.rotation = Quaternion.Euler(currentXRotation, transform.rotation.eulerAngles.y, 0.0f);
     }
 }
