43a4bd0c1b0ed3232d7d9ae693cd9ba0ecd66d30
ljs9711 <32507881+ljs9711@users.noreply.github.com>
2024-01-23 17:14:52 +0900

    feat: weapon sway

--- Fire_simulator/Assets/03_Scripts/PlayerController.cs
+++ Fire_simulator/Assets/03_Scripts/PlayerController.cs
@@ -183,7 +183,7 @@ public class PlayerController : MonoBehaviour
     //지면체크
     private void IsGround()
     {                                                                //캡슐콜라이더 영역의 Y 의 절반+ 0.1f(계단,대각등 판정 위해)
-        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f); //고정된 좌표 사용하기위해 Vector3 사용
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 1f); //고정된 좌표 사용하기위해 Vector3 사용
         theCrosshair.JumpingAnimation(!isGround);
     }
 
