9b649cc29bc2f988b06df0adb3fbd7cb9d2ac094
ljs9711 <32507881+ljs9711@users.noreply.github.com>
2024-01-25 19:28:26 +0900

    feat: sound manager

--- Fire_simulator/Assets/03_Scripts/Rock.cs
+++ Fire_simulator/Assets/03_Scripts/Rock.cs
@@ -21,19 +21,15 @@ public class Rock : MonoBehaviour
     [SerializeField]
     private GameObject go_effect_prefabs; //채굴 이펙트
 
+    //필요한 사운드 이름
     [SerializeField]
-    private AudioSource audioSource;
+    private string strike_Sound;
     [SerializeField]
-    private AudioClip effect_Sound;
-    [SerializeField]
-    private AudioClip effect_Sound2;
-
-
+    private string destroy_Sound;
 
     public void Mining()
     {
-        audioSource.clip = effect_Sound;
-        audioSource.Play();
+        SoundManager.instance.PlaySE(strike_Sound);
 
         var clone = Instantiate(go_effect_prefabs, col.bounds.center, Quaternion.identity);
         Destroy(clone, destroyTime);
@@ -45,8 +41,7 @@ public class Rock : MonoBehaviour
 
     private void Destruction()
     {
-        audioSource.clip = effect_Sound2;
-        audioSource.Play();
+        SoundManager.instance.PlaySE(destroy_Sound);
 
         col.enabled = false;
         Destroy(go_rock);
