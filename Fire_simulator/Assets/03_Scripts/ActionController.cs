e4fedeb44f404db2439c1fa01586d95558f036b6
ljs9711 <32507881+ljs9711@users.noreply.github.com>
2024-01-29 09:26:43 +0900

    feat: item show text

--- Fire_simulator/Assets/03_Scripts/ActionController.cs
+++ Fire_simulator/Assets/03_Scripts/ActionController.cs
@@ -1,18 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float range; //획득 가능한 최대거리
 
-    // Update is called once per frame
+    private bool pickupActivated = false; //획득 가능하면 true
+
+    private RaycastHit hitInfo; //충돌체 정보 저장
+
+    //아이템 레이어에만 반응하도록 레이어 설정
+    [SerializeField]
+    private LayerMask layerMask;
+
+    //필요한 컴포넌트
+    [SerializeField]
+    private Text actionText;
+
+
+
     void Update()
     {
-        
+        CheckItem();
+        TryAction();
+    }
+
+    private void TryAction()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            CheckItem();
+            CanPickup();
+        }
+    }
+
+    private void CanPickup()
+    {
+        if(pickupActivated )
+        {
+            if(hitInfo.transform != null)
+            {
+                Debug.Log(hitInfo.transform.GetComponent<ItemPickup>().item.itemName + " 획득했습니다");
+                Destroy(hitInfo.transform.gameObject);
+                InfoDisappear();
+            }
+        }
+    }
+
+    //아이템 체크
+    private void CheckItem()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInfo, range, layerMask))
+        {
+            if (hitInfo.transform.tag == "Item")
+            {
+                ItemInfoAppear();
+            }
+        }
+        else
+            InfoDisappear();
+    }
+
+    //아이템 획득 텍스트 생겨남
+    private void ItemInfoAppear()
+    {
+        pickupActivated = true;
+        actionText.gameObject.SetActive(true);
+        actionText.text = hitInfo.transform.GetComponent<ItemPickup>().item.itemName + " 획득 " + "<coler=yellow>" + "(E)" + "</color>";
+    }
+
+    //아이템 획득 텍스트 사라짐
+    private void InfoDisappear()
+    {
+        pickupActivated = false;
+        actionText.gameObject.SetActive(false);
     }
 }
--- Fire_simulator/Assets/03_Scripts/Item.cs
+++ Fire_simulator/Assets/03_Scripts/Item.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NEW Item", menuName = "NEW Item/item")]
 public class Item : ScriptableObject //스크립트를 안붙혀도 됨
 {
-    public string itemNmae; //아이템의 이름
+    public string itemName; //아이템의 이름
     public ItemType itemType; //아이템의 유형
     public Sprite itemImage; //아이템의 이미지
     public GameObject itemPrefab; // 아이템의 프리팹
